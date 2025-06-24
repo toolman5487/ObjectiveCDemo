286da000283b4eead38d10242f807d69249442b1
ToolManHsu <willy548798@gmail.com>
2025-06-24 22:31:52 +0800

    調整 IntroViewController 顯示方式，使用 Masonry 套件優化排版

--- ObjectiveCDemo/IntroViewController.m
+++ ObjectiveCDemo/IntroViewController.m
@@ -6,8 +6,11 @@
 //
 
 #import "IntroViewController.h"
+#import "Masonry.h"
 
 @interface IntroViewController ()
+
+@property (nonatomic, strong) UILabel *infoLabel;
 
 @end
 
@@ -17,19 +20,33 @@
     [super viewDidLoad];
     self.view.backgroundColor = [UIColor whiteColor];
     self.title = @"關於我";
-
-    UILabel *label = [[UILabel alloc] initWithFrame:CGRectMake(20, 150, self.view.bounds.size.width - 40, 200)];
-    label.numberOfLines = 0;
-    label.textAlignment = NSTextAlignmentLeft;
-    label.text = @"我是 Willy"
-                 "熟悉 App 軟體開發：\n\n"
-                 "語言與工具：Swift, Xcode, Git / GitHub, SPM, CocoaPods\n"
-                 "架構與設計：UIKit, SwiftUI, MVC / MVVM, Combine\n"
-                 "資料處理與串接：RESTful API, URLSession / JSONDecoder, Firebase Authentication, CoreData, Postman\n"
-                 "測試經驗：XCTest（單元與 UI 測試）, Combine 非同步資料流測試\n"
-                 "設計與用戶體驗：iOS Human Interface Guidelines";
-    [self.view addSubview:label];
+    [self setupUI];
+    [self setupLayout];
 }
+
+- (void)setupUI {
+    self.infoLabel = [[UILabel alloc] init];
+    self.infoLabel.numberOfLines = 0;
+    self.infoLabel.textAlignment = NSTextAlignmentLeft;
+    self.infoLabel.text = @"我是 Willy\n\n"
+                           "熟悉 App 軟體開發：\n\n"
+                           "語言與工具：Swift, Xcode, Git / GitHub, SPM, CocoaPods\n"
+                           "架構與設計：UIKit, SwiftUI, MVC / MVVM, Combine\n"
+                           "資料處理與串接：RESTful API, URLSession / JSONDecoder, Firebase Authentication, CoreData, Postman\n"
+                           "測試經驗：XCTest（單元與 UI 測試）, Combine 非同步資料流測試\n"
+                           "設計與用戶體驗：iOS Human Interface Guidelines";
+    self.infoLabel.translatesAutoresizingMaskIntoConstraints = NO;
+    [self.view addSubview:self.infoLabel];
+}
+
+- (void)setupLayout {
+    [self.infoLabel mas_makeConstraints:^(MASConstraintMaker *make) {
+        make.top.equalTo(self.view).offset(150);
+        make.left.equalTo(self.view).offset(20);
+        make.right.equalTo(self.view).offset(-20);
+    }];
+}
+
 /*
 #pragma mark - Navigation
 
